8fd1aa00d3ae639ea1342bb6777bb271f0017d40
yazawa-urakata <yazawa@urakata-hack.com>
2025-12-02 19:20:18 +0900

    chore: remove unnecessary files

--- src/pages/Content/images/popup/images.ts
+++ src/pages/Content/images/popup/images.ts
@@ -8,22 +8,8 @@ const MicOffIcon = `${URL}/mic-off.svg`;
 const CameraOnIcon = `${URL}/camera-on.svg`;
 const CameraOffIcon = `${URL}/camera-off.svg`;
 const CheckWhiteIcon = `${URL}/check-white.svg`;
-const ScreenTabOn = `${URL}/screen-tab-on.svg`;
-const ScreenTabOff = `${URL}/screen-tab-off.svg`;
-const TempFigma = `${URL}/temp/figma.webp`;
-const TempTwitter = `${URL}/temp/twitter.webp`;
-const TempDesignSystem = `${URL}/temp/designsystem.webp`;
-const TempMarketing = `${URL}/temp/marketing.webp`;
-const TempSubstack = `${URL}/temp/substack.webp`;
-const CopyLinkIcon = `${URL}/copy-link.svg`;
-const HandleControl = `${URL}/canvas/handle.png`;
-const RotateControl = `${URL}/canvas/rotate.png`;
-const MiddleHandleControl = `${URL}/canvas/middle-handle.png`;
-const MiddleHandleControlV = `${URL}/canvas/middle-handle-v.png`;
-const CameraOffBlue = `${URL}/camera-off-blue.svg`;
 const MicOffBlue = `${URL}/mic-off-blue.svg`;
 const DropdownGroup = `${URL}/dropdown-group.svg`;
-const PlaceholderThumb = `${URL}/placeholder-thumb.png`;
 const CloseWhiteIcon = `${URL}/close-white.svg`;
 
 export {
@@ -33,21 +19,7 @@ export {
   CameraOnIcon,
   CameraOffIcon,
   CheckWhiteIcon,
-  ScreenTabOn,
-  ScreenTabOff,
-  TempFigma,
-  TempTwitter,
-  TempDesignSystem,
-  TempMarketing,
-  TempSubstack,
-  CopyLinkIcon,
-  HandleControl,
-  RotateControl,
-  MiddleHandleControl,
-  MiddleHandleControlV,
-  CameraOffBlue,
   MicOffBlue,
   DropdownGroup,
-  PlaceholderThumb,
   CloseWhiteIcon,
 };
